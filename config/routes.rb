1c73ee401cc895c8c0fce44d8aa02970caa2a44a
Marius Retzmann <marc.retzmann@web.de>
2023-05-28 18:57:24 +0800

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+end
+
+Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask'
+end
+
+
+Rails.application.routes.draw do
+  get 'answer', to: 'questions#answer'
+  post 'answer', to: 'questions#submit_answer'
 end
